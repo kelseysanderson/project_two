ba09ba7414f6b6ec7e479008ebcdd4533720bb1d
Nathan Hentges <nathanjhentges@gmail.com>
2021-04-21 11:23:54 -0500

    renamed config js file, changed config to have JAWSDB

---
+++ config/config.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let connection;
+
+// create connection to our db
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = connection;
