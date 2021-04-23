d11e67512ec8d2230e548cb72622e4b50e8273f7
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-04-23 10:25:33 -0500

    added plant seed file and test data on homeRoutes.js

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -3,24 +3,18 @@ const fetch = require('node-fetch');
 const { Plant, User, Garden } = require('../models');
 
 router.get('/', async (req, res) => {
-  const featuredplant = {
-    featuredplant: [
-      {
-        image_url: 'https://via.placeholder.com/200', name: 'plant'
-      },
-      {
-        image_url: 'https://via.placeholder.com/300', name: 'plant'
-      }
-    ]
-  };
-  console.log(featuredplant);
-  console.log('I am at the homepage?');
-  res.render('homepage', { featuredplant });
+  const dishes = 
+    {
+      dish_name: 'French Bread with Brie Cheese',
+      description: 'French baguette with warm brie',
+    }
+  
+  // console.log(featuredplant);
+  res.render('homepage', {dishes} );
 });
 
 // login
 router.get('/login', async (req, res) => {
-  console.log('i am logged in');
   res.render('login');
 });
 
@@ -37,6 +31,7 @@ router.get('/search', async (req, res) => {
 router.get('/search/:query', async (req, res, next) => {
   const searchResult = await fetch(`https://trefle.io/api/v1/plants/search?q=${req.params.query}&token=oAC1gBhoTITc0LexBLXeOfr4ix2qc-DiGQXk1c3b2Rs`);
   const searchJson = await searchResult.json();
+
   // convert searchJson to object to pass to handlebars
   req.searchResults = searchJson;
   // res.render('search', data)
