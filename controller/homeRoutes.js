19ef2e6b3ac441dbaeb5fd3d5910754b6c6a5aa7
Nathan Hentges <nathanjhentges@gmail.com>
2021-04-22 20:22:18 -0500

    added node-fetch package, fixed api fetch and added route

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,32 +1,43 @@
 const router = require('express').Router();
+const fetch = require('node-fetch');
 const { Plant, User, Garden } = require('../models');
 
 router.get('/', async (req, res) => {
-  let featuredplant = {"featuredplant":[{
-    image_url: "https://via.placeholder.com/200", name: "plant"
-  },
-  {
-    image_url: "https://via.placeholder.com/300", name: "plant"
-  }
-
-
-  ]};
+  const featuredplant = {
+    featuredplant: [
+      {
+        image_url: 'https://via.placeholder.com/200', name: 'plant'
+      },
+      {
+        image_url: 'https://via.placeholder.com/300', name: 'plant'
+      }
+    ]
+  };
   console.log(featuredplant);
-  res.render('homepage', {featuredplant});
+  res.render('homepage', { featuredplant });
 });
 
 // login
 router.get('/login', async (req, res) => {
   res.render('login');
 });
+
 // my garden/favourites
 router.get('/mygarden', async (req, res) => {
   res.render('mygarden');
 });
+
 // search results
 router.get('/search', async (req, res) => {
   res.render('search');
 });
+
+router.get('/search/:query', async (req, res) => {
+  const searchResult = await fetch(`https://trefle.io/api/v1/plants/search?q=${req.params.query}&token=oAC1gBhoTITc0LexBLXeOfr4ix2qc-DiGQXk1c3b2Rs`);
+  const searchJson = await searchResult.json();
+  res.json(searchJson);
+});
+
 // plant page
 router.get('/plant', async (req, res) => {
   res.render('plant');
